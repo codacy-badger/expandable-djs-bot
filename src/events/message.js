e5e61c2461bb2805ec1d5c8d1f0f5b7727a9b8cd
Angel <69059762+AngelNull@users.noreply.github.com>
2021-01-13 07:46:45 -0800

    Fix(message.js): Bot responds to any prefix given (#21)
    Co-authored-by: AngelicalDev <69059762+angelicaldev@users.noreply.github.com>

--- src/events/message.js
+++ src/events/message.js
@@ -23,53 +23,53 @@ module.exports = async (client, message) => {
         setTimeout(() => {
             ratelimitCooldown.delete(message.author.id);
         }, 630);
-    }
 
-    /* Check if the user executing the command is the owner of the bot, if not, return it */
-    if (command.devOnly && config.ownerID != message.author.id) return message.channel.send(tr.translate('NO_PERMISSION', lang, message.author));
+        /* Check if the user executing the command is the owner of the bot, if not, return it */
+        if (command.devOnly && config.ownerID != message.author.id) return message.channel.send(tr.translate('NO_PERMISSION', lang, message.author));
 
-    /* If the command has a permission object, and the user does not have that permission; deny the user from executing the command and return. */
-    if (command.permission && !message.member.hasPermission(command.permission)) return message.channel.send(tr.translate('NO_PERMISSION', lang, message.author));
+        /* If the command has a permission object, and the user does not have that permission; deny the user from executing the command and return. */
+        if (command.permission && !message.member.hasPermission(command.permission)) return message.channel.send(tr.translate('NO_PERMISSION', lang, message.author));
 
-    /* If the command requires args and the user does not pass them; tell the user the correct usage of the command and return */
-    if (command.args && !args.length) {
-        /* Send the error message; delete the authors message and error message after elapsed time */
-        let errmsg = await message.channel.send(tr.translate('INCORRECT_USAGE', lang, config.prefix, commandName, command.usage));
-        message.delete({ timeout: 6000 });
-        return errmsg.delete({ timeout: 8000 });
-    }
-
-    /* Per-Command cooldown is handled here, if a user uses the same command before the cooldown has ended, deny the user from using the command */
-    if (!cooldowns.has(command.name)) {
-        cooldowns.set(command.name, new Collection());
-    }
-
-    const now = Date.now();
-    const timestamps = cooldowns.get(command.name);
-    const cooldownAmount = (command.cooldown || 3) * 1000;
-
-    if (timestamps.has(message.author.id)) {
-        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-        if (now < expirationTime) {
-            const timeLeft = (expirationTime - now) / 1000;
-            let cooldownMessage = await message.channel.send(tr.translate('ON_COOLDOWN', lang, message.author, timeLeft.toFixed(1), command.name));
-            message.delete({ timeout: 4000 }).catch(() => {});
-            return cooldownMessage.delete({ timeout: 2500 }).catch(() => {
-                return;
-            });
+        /* If the command requires args and the user does not pass them; tell the user the correct usage of the command and return */
+        if (command.args && !args.length) {
+            /* Send the error message; delete the authors message and error message after elapsed time */
+            let errmsg = await message.channel.send(tr.translate('INCORRECT_USAGE', lang, config.prefix, commandName, command.usage));
+            message.delete({ timeout: 6000 });
+            return errmsg.delete({ timeout: 8000 });
         }
-    }
 
-    try {
-        timestamps.set(message.author.id, now);
-        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+        /* Per-Command cooldown is handled here, if a user uses the same command before the cooldown has ended, deny the user from using the command */
+        if (!cooldowns.has(command.name)) {
+            cooldowns.set(command.name, new Collection());
+        }
 
-        /* Execute the command */
-        command.execute(message, lang, tr, args);
-    } catch (error) {
-        /* If we end up here; that means the command has failed to execute properly. */
-        console.error(error);
-        message.channel.send(tr.translate('ERROR_OUTPUT', lang));
+        const now = Date.now();
+        const timestamps = cooldowns.get(command.name);
+        const cooldownAmount = (command.cooldown || 3) * 1000;
+
+        if (timestamps.has(message.author.id)) {
+            const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+            if (now < expirationTime) {
+                const timeLeft = (expirationTime - now) / 1000;
+                let cooldownMessage = await message.channel.send(tr.translate('ON_COOLDOWN', lang, message.author, timeLeft.toFixed(1), command.name));
+                message.delete({ timeout: 4000 }).catch(() => {});
+                return cooldownMessage.delete({ timeout: 2500 }).catch(() => {
+                    return;
+                });
+            }
+        }
+
+        try {
+            timestamps.set(message.author.id, now);
+            setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+            /* Execute the command */
+            command.execute(message, lang, tr, args);
+        } catch (error) {
+            /* If we end up here; that means the command has failed to execute properly. */
+            console.error(error);
+            message.channel.send(tr.translate('ERROR_OUTPUT', lang));
+        }
     }
 };
